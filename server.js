478501777bff831016ff6ef4687220ff0a3abc70
Colin <colingoodale@gmail.com>
2018-10-10 23:51:50 -0500

    Build out for backend done

--- server.js
+++ server.js
@@ -1,82 +1,163 @@
 // Dependencies
+
 var express = require("express");
-var mongojs = require("mongojs");
-// Require axios and cheerio. This makes the scraping possible
-var axios = require("axios");
+var method = require("method-override");
+var body = require("body-parser");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var logger = require("morgan");
 var cheerio = require("cheerio");
+var request = require("request");
 
-// Initialize Express
-var app = express();
+// Mongoose
 
-// Database configuration
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+var Note = require("./models/Note");
+var Article = require("./models/Article");
+var databaseUrl = 'mongodb://localhost/nyt';
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+    mongoose.connect(databaseUrl);
+};
+
+mongoose.Promise = Promise;
+var db = mongoose.connection;
+
 db.on("error", function (error) {
-    console.log("Database Error:", error);
+    console.log("Mongoose Error: ", error);
 });
 
-// Main route (simple Hello World Message)
+db.once("open", function () {
+    console.log("Mongoose connection successful.");
+});
+
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// app set-ups
+
+app.use(logger("dev"));
+app.use(express.static("public"));
+app.use(body.urlencoded({ extended: false }));
+app.use(method("_method"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(port, function () {
+    console.log("Listening on port " + port);
+})
+
+// Routes
+
 app.get("/", function (req, res) {
-    res.send("Hello world");
-});
-
-// Retrieve data from the db
-app.get("/all", function (req, res) {
-    // Find all results from the scrapedData collection in the db
-    db.scrapedData.find({}, function (error, found) {
-        // Throw any errors to the console
-        if (error) {
-            console.log(error);
+    Article.find({}, null, { sort: { created: -1 } }, function (err, data) {
+        if (data.length === 0) {
+            res.render("placeholder", { message: "There's nothing scraped yet. Please click \"Scrape For Newest Articles\" for fresh and delicious news." });
         }
-        // If there are no errors, send the data to the browser as json
         else {
-            res.json(found);
+            res.render("index", { articles: data });
         }
     });
 });
 
-// Scrape data from one site and place it into the mongodb db
 app.get("/scrape", function (req, res) {
-    // Make a request via axios for the news section of `ycombinator`
-    axios.get("https://news.ycombinator.com/").then(function (response) {
-        // Load the html body from axios into cheerio
-        var $ = cheerio.load(response.data);
-        // For each element with a "title" class
-        $(".title").each(function (i, element) {
-            // Save the text and href of each link enclosed in the current element
-            var title = $(element).children("a").text();
-            var link = $(element).children("a").attr("href");
-
-            // If this found element had both a title and a link
-            if (title && link) {
-                // Insert the data in the scrapedData db
-                db.scrapedData.insert({
-                    title: title,
-                    link: link
-                },
-                    function (err, inserted) {
-                        if (err) {
-                            // Log the error if one is encountered during the query
-                            console.log(err);
-                        }
-                        else {
-                            // Otherwise, log the inserted data
-                            console.log(inserted);
-                        }
+    request("https://www.nytimes.com/section/world", function (error, response, html) {
+        var $ = cheerio.load(html);
+        var result = {};
+        $("div.story-body").each(function (i, element) {
+            var link = $(element).find("a").attr("href");
+            var title = $(element).find("h2.headline").text().trim();
+            var summary = $(element).find("p.summary").text().trim();
+            var img = $(element).parent().find("figure.media").find("img").attr("src");
+            result.link = link;
+            result.title = title;
+            if (summary) {
+                result.summary = summary;
+            };
+            if (img) {
+                result.img = img;
+            }
+            else {
+                result.img = $(element).find(".wide-thumb").find("img").attr("src");
+            };
+            var entry = new Article(result);
+            Article.find({ title: result.title }, function (err, data) {
+                if (data.length === 0) {
+                    entry.save(function (err, data) {
+                        if (err) throw err;
                     });
+                }
+            });
+        });
+        console.log("Scrape finished.");
+        res.redirect("/");
+    });
+});
+
+app.get("/saved", function (req, res) {
+    Article.find({ issaved: true }, null, { sort: { created: -1 } }, function (err, data) {
+        if (data.length === 0) {
+            res.render("placeholder", { message: "You have not saved any articles yet. Try to save some delicious news by simply clicking \"Save Article\"!" });
+        }
+        else {
+            res.render("saved", { saved: data });
+        }
+    });
+});
+
+app.get("/:id", function (req, res) {
+    Article.findById(req.params.id, function (err, data) {
+        res.json(data);
+    })
+})
+
+app.post("/search", function (req, res) {
+    console.log(req.body.search);
+    Article.find({ $text: { $search: req.body.search, $caseSensitive: false } }, null, { sort: { created: -1 } }, function (err, data) {
+        console.log(data);
+        if (data.length === 0) {
+            res.render("placeholder", { message: "Nothing has been found. Please try other keywords." });
+        }
+        else {
+            res.render("search", { search: data })
+        }
+    })
+});
+
+app.post("/save/:id", function (req, res) {
+    Article.findById(req.params.id, function (err, data) {
+        if (data.issaved) {
+            Article.findByIdAndUpdate(req.params.id, { $set: { issaved: false, status: "Save Article" } }, { new: true }, function (err, data) {
+                res.redirect("/");
+            });
+        }
+        else {
+            Article.findByIdAndUpdate(req.params.id, { $set: { issaved: true, status: "Saved" } }, { new: true }, function (err, data) {
+                res.redirect("/saved");
+            });
+        }
+    });
+});
+
+app.post("/note/:id", function (req, res) {
+    var note = new Note(req.body);
+    note.save(function (err, doc) {
+        if (err) throw err;
+        Article.findByIdAndUpdate(req.params.id, { $set: { "note": doc._id } }, { new: true }, function (err, newdoc) {
+            if (err) throw err;
+            else {
+                res.send(newdoc);
             }
         });
     });
-
-    // Send a "Scrape Complete" message to the browser
-    res.send("Scrape Complete");
 });
 
-
-// Listen on port 3000
-app.listen(3000, function () {
-    console.log("App running on port 3000!");
-});
+app.get("/note/:id", function (req, res) {
+    var id = req.params.id;
+    Article.findById(id).populate("note").exec(function (err, data) {
+        res.send(data.note);
+    })
+})
